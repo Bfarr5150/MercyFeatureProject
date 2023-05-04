46f5a52843c255d05efcd6bc63c7f7ba0ad8e94b
Brad Farris <xtraponagex@live.com>
2023-05-04 04:12:13 -0700

    PlayerController GA Progress (Not Complete)
    - GA Coroutine created
    - created methods to check for ally presence in player's view/in range of player
    - added comments to methods
    - rearranged variables to fit appropriate categories
    - GA is not complete/working yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,56 @@ using System.Data.SqlTypes;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    // GameObject & components
     public Rigidbody rb;
     public GameObject camHolder;
-    public float speed, camSense, maxVelocity, jumpVelocity, GAVelocity, superJumpVel;
-    private Vector2 move, look, jump, guardianAngel, superJump;
+    public GameObject ally;
+
+    // Base movement variables
+    public float speed, camSense, maxVelocity, jumpVelocity;
+    private Vector2 move, look;
     private float lookRotation;
-    private bool grounded, inRange, GAActive;
+    private bool grounded;
+
+    // Mercy mechanic variables
+    public float GASpeed, SuperJumpSpeed;
+    public float GAEndThreshold = 0.5f;
+    public float allyInViewThreshold = 30f;
+    public float allyInRangeThreshold = 10f;
+    private bool GAActive = false;
+
 
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;    
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("GAActive state: " + GAActive);
+        Debug.Log("Ally in view: " + allyInFOV());
+
     }
 
 
     private void FixedUpdate()
     {
         Move();
+        if (allyInFOV() == true)
+        {
+            Debug.Log("Ally is in view");
+        }
+        if (allyInRange() == true)
+        {
+            Debug.Log("Ally is in range");
+        }
+        /*
+        if (!GAActive)
+        {
+            if(allyInRange() && allyInFOV())
+            {
+                ActivateGA();
+                Debug.Log("GA activated");
+            }
+        }
+        */
     }
 
     void LateUpdate()
@@ -45,6 +77,15 @@ public class PlayerController : MonoBehaviour
     public void OnJump(InputAction.CallbackContext context)
     {
         Jump();
+    }
+
+    public void OnGuardianAngel(InputAction.CallbackContext context)
+    {
+        if (!GAActive && allyInFOV() && allyInRange() && context.performed)
+        {
+                StartCoroutine(ActivateGA());
+                Debug.Log("GA activated");
+        }        
     }
 
     void Move()
@@ -92,9 +133,51 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(jumpVelocityCurr, ForceMode.VelocityChange);
     }
 
+    // used to determine the state of the player, in relation to being on the ground
+        // if the player is touching the ground, this allows them to jump
     public void SetGrounded(bool state)
     {
         grounded = state;
     }
+
+    // Coroutine for Guardian Angel ability activation
+    IEnumerator ActivateGA()
+    {
+        GAActive = true;
+
+        Vector3 startPos = transform.position;
+        Vector3 endPos = ally.transform.position;
+
+        float GADistance = Vector3.Distance(startPos, endPos);
+        float GADuration = GADistance / GASpeed;
+
+        float GAFlyTime = 0f;
+        while (GAFlyTime < GADuration)
+        {
+            rb.MovePosition(Vector3.Lerp(startPos, endPos, GAFlyTime / GADuration));
+            GAFlyTime += Time.deltaTime;
+            yield return null;
+        }
+
+        rb.MovePosition(endPos);
+
+        GAActive = false;
+    }
+
+    // Check for ally's position in player's FOV
+    bool allyInFOV()
+    {
+        Vector3 allyDirection = ally.transform.position - transform.position;
+        float allyPosAngle = Vector3.Angle(transform.forward, allyDirection);
+        //Debug.Log("ally in FOV");
+        return allyPosAngle <= allyInViewThreshold;
+    }
+
+    // Check if ally is within range for player to use GA
+    bool allyInRange()
+    {
+        return Vector3.Distance(transform.position, ally.transform.position) <= allyInRangeThreshold;
+    }
+
 
 }
