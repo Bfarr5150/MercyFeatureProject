51d39abd508e291ce1d4b53f28b84ccc12d459ab
Brad Farris <bfarris1@mail.csuchico.edu>
2023-05-09 07:49:59 -0700

    Scene Management
    - Start/Instructions scene implemented
    - Need to add Angelic Descent description
    - Indexed scenes in Build Settings
    - Finished SceneManage.cs to function for switching scenes

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene(int scene)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(scene);
     }
 }
